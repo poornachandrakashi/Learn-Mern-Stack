37e29f73f813bbd4fed0b6609ca4a52bafd33195
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.com>
2020-08-20 16:57:33 -0400

    Database Connected successfully

--- config/db.js
+++ config/db.js
@@ -2,3 +2,19 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
+
+
+
+const connectDB = async() =>{
+    try{
+        await mongoose.connect(db);
+        console.log("Mongo DB Successfully connected!!!");
+    }catch(err){
+        console.error(err.message);
+        //Exiting the process
+        process.exit(1);
+    }
+}
+
+
+module.exports = connectDB;
