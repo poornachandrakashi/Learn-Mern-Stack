57c03e9529080c0c55f27c88ab0285aaf1298bd0
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.com>
2020-08-20 17:04:50 -0400

    Completed Mongo Db connections

--- config/db.js
+++ config/db.js
@@ -7,8 +7,10 @@ const db = config.get('mongoURI');
 
 const connectDB = async() =>{
     try{
-        await mongoose.connect(db);
-        console.log("Mongo DB Successfully connected!!!");
+        await mongoose.connect(db,{
+            useNewUrlParser:true
+        });
+        console.log("Hurray Mongo DB Successfully connected!!!");
     }catch(err){
         console.error(err.message);
         //Exiting the process
