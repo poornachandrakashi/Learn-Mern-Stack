9618ccc31a2d8bbb9d9a81df0329342f555fee95
poornachandra kashi <44542685+poornachandrakashi@users.noreply.github.com>
2020-08-23 16:22:56 +0530

    Token Created

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 //Using Mong Model
@@ -34,38 +36,49 @@ async (req,res) => {
 
 
     // Gets user Gravitar
-    const avatar = gravatar.url(email, {
-        s: '200',
-        r: 'pg',
-        d: 'mm'
-    })
+        const avatar = gravatar.url(email, {
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+        })
 
-    user = new User({
-        name,
-        email,
-        avatar,
-        password //its not encrypted yet
-    });
-    //Encrypt password   using bycrpt
-    const salt = await bcrypt.genSalt(10);
+        user = new User({
+            name,
+            email,
+            avatar,
+            password //its not encrypted yet
+        });
+        //Encrypt password   using bycrpt
+        const salt = await bcrypt.genSalt(10);
 
 
-    user.password = await bcrypt.hash(password,salt);
+        user.password = await bcrypt.hash(password,salt);
 
-    await user.save();
+        await user.save();
 
-    //Return JsonWebtokens
+        //Return JsonWebtokens
 
-    res.send('User Registered!!!');
+        //Create a payload
+        
+        const payload = {
+            user:{
+                id: user.id
+            }
+        };
+
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            { expiresIn: 360000 },
+            (err, token) => {
+            if(err) throw err;
+            res.json({ token });
+        });
     //console.log(req.body);
     }catch(err){
         console.log(err.message);
         res.send(500).send('Server Error');
     }
-
-
-    
-    
 });
 
 module.exports = router;
